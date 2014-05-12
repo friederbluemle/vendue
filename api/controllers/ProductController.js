3692885b41e475253a09072f823e9b4e0b08262e
Frieder Bluemle <frieder.bluemle@gmail.com>
2014-05-12 01:40:57 -0700

    Add views and update controller for products

--- api/controllers/ProductController.js
+++ api/controllers/ProductController.js
@@ -16,15 +16,87 @@
  */
 
 module.exports = {
-    
-  
-
 
   /**
    * Overrides for the settings in `config/controllers.js`
    * (specific to ProductController)
    */
-  _config: {}
+  _config: {},
 
-  
+  'new': function(req, res) {
+    res.view();
+  },
+
+  create: function (req, res, next) {
+    Product.create(req.params.all(), function productCreated(err, product) {
+      if (err) {
+        console.log(err);
+        req.session.flash = {
+          err: err
+        }
+
+        return res.redirect('/product/new');
+      }
+
+      res.redirect('/product/show/' + product.id);
+    });
+  },
+
+  // render the profile view (e.g. /views/show.ejs)
+  show: function(req, res, next) {
+    Product.findOne(req.param('id'), function foundProduct(err, product) {
+      if (err) return next(err);
+      if (!product) return next();
+      res.view({
+        product: product
+      });
+    });
+  },
+
+  index: function(req, res, next) {
+    // Get an array of all products in the Product collection(e.g. table)
+    Product.find(function foundProducts(err, products) {
+      if (err) return next(err);
+      // pass the array down to the /views/index.ejs page
+      res.view({
+        products: products
+      });
+    });
+  },
+
+  // render the edit view (e.g. /views/edit.ejs)
+  edit: function(req, res, next) {
+    // Find the product from the id passed in via params
+    Product.findOne(req.param('id'), function foundProduct(err, product) {
+      if (err) return next(err);
+      if (!product) return next('Product doesn\'t exist.');
+      res.view({
+        product: product
+      });
+    });
+  },
+
+  // process the info from edit view
+  update: function(req, res, next) {
+    Product.update(req.param('id'), req.params.all(), function productUpdated(err) {
+      if (err) {
+        return res.redirect('/product/edit/' + req.param('id'));
+      }
+      res.redirect('/product/show/' + req.param('id'));
+    });
+  },
+
+  destroy: function(req, res, next) {
+    Product.findOne(req.param('id'), function foundProduct(err, product) {
+      if (err) return next(err);
+      if (!product) return next('Product doesn\'t exist.');
+
+      Product.destroy(req.param('id'), function productDestroyed(err) {
+        if (err) return next(err);
+      });
+
+      res.redirect('/product');
+    });
+  }
+
 };
